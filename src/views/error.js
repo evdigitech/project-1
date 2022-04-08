d6e0c2cc8cc8ef13f6ad0085b816e8a62f44a295
Sagar Soni <dusksoni@gmail.com>
2022-04-08 13:47:49 +0530

    delete all

--- src/views/error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Error() {
-    return (
-        <div>
-            <h1 className=" text-center text-red-700 font-extrabold">Page Not Found</h1>
-        </div>
-    )
-}
